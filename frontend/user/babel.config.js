5f66441714a0f7f755b4ee0e6f3536ed03842062
tuanbui0509 <tuanbui0509@example.com>
2025-02-01 11:53:52 +0700

    Update file

--- frontend/user/babel.config.js
+++
@@ -1,4 +0,0 @@
-// babel.config.js
-module.exports = {
-    presets: ['next/babel', '@babel/preset-env', '@babel/preset-react'],
-};
--- frontend/user/src/components/Layout.tsx
+++ frontend/user/src/components/Layout.tsx
@@ -1,7 +1,8 @@
 import Header from "./Header";
 import Footer from "./Footer";
 
-export default function Layout({ children}) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function Layout({ children}:any) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
